d324edca80487ea7f1c798c425dedc7f9f77d08a
Kevin Clark <milligna@gmail.com>
2020-02-01 21:32:14 +1100

    public methods for crash controller

--- Raptor Prison Ship 4/Assets/_Project/Scripts/CrashController.cs
+++ Raptor Prison Ship 4/Assets/_Project/Scripts/CrashController.cs
@@ -9,31 +9,31 @@ public class CrashController : MonoBehaviour
     public GameObject explosionEffectPrefab;
     
 
-    void SetCrashScreen()
+    public void SetCrashScreen()
     {
         // Set crash screen for the computer object
     }
 
-    void StartSmokingEffect()
+   public void StartSmokingEffect()
     {
         // Activate the Smoke object in the Prefab
     
         smokeEffect.SetActive(true);
     }
 
-    void TriggerCountdownAudio()
+    public void TriggerCountdownAudio()
     {
         // Activate the Countdown Audio Noise in the Prefab
 
     }
 
-    void TriggerExplosionEffect()
+    public void TriggerExplosionEffect()
     {
         // instantiate Explosion audio and prefab, kill it and end the game
         Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
     }
 
-    void CancelCrashEffects()
+   public void CancelCrashEffects()
     {
         // Deactivate all objects in the prefab
         smokeEffect.SetActive(false);
