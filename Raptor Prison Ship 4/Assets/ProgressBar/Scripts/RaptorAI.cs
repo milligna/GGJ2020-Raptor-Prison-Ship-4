e0653b584dae2ff022cab6e32c78060874c2fdc3
joel <jrattur@gmail.com>
2020-02-01 17:39:43 +1100

    Added progressbar

---
+++ Raptor Prison Ship 4/Assets/ProgressBar/Scripts/RaptorAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RaptorAI : MonoBehaviour
+{
+
+	public Computer targettedComputer;
+	[SerializeField]
+	private GameObject targettedLocation;
+	[SerializeField]
+	private NavMeshAgent raptorAgent;
+
+	private ComputerManager CM;
+
+	[SerializeField]
+	private NonComputer [] nonComputerLocations;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		CM = FindObjectOfType<ComputerManager> ();
+		nonComputerLocations = FindObjectsOfType<NonComputer> ();
+    }
+
+
+	public void FindNewTarget ()
+	{
+		targettedComputer = CM.GetRaptorTarget ();
+
+		if (targettedComputer == null) {
+			targettedLocation = nonComputerLocations [Random.Range (0, nonComputerLocations.Length)].gameObject;
+		} else {
+			targettedLocation = targettedComputer.gameObject;
+		}
+
+		raptorAgent.SetDestination (targettedLocation.transform.position);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (targettedLocation == null) {
+
+			FindNewTarget ();
+
+		}
+    }
+}
---
+++ Raptor Prison Ship 4/Assets/SetProgressBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetProgressBar : MonoBehaviour
+{
+
+    public ProgressBar progressBar;
+    public ProgressBarCircle progressBarCircle;
+
+    [SerializeField]
+    private float progress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressBar.BarValue = progress;
+        progressBarCircle.BarValue = progress;
+    }
+}
