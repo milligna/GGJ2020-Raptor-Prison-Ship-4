ee999efe613908f623e1abf894724fc1440a0915
Kevin Clark <milligna@gmail.com>
2020-02-02 21:36:46 +1100

    Returned Crawl to the menu controller

--- Raptor Prison Ship 4/Assets/MenuController.cs
+++ Raptor Prison Ship 4/Assets/MenuController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
 
-    public void StartGame() { SceneManager.LoadScene("Main"); }
+    public void StartGame() { SceneManager.LoadScene("Crawl"); }
 
     public void QuitGame() { Application.Quit(); }
 }
