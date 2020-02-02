bfb39d929bb84a8ed3ed637bcd711a6fe3f56cde
Peter J Armstrong <ashen_s@yahoo.com.au>
2020-02-02 11:54:39 +1100

    A small change that should be over written, but at least shows where in the code the new stuff should go.

---
+++ Raptor Prison Ship 4/Assets/_Project/Scripts/DoorControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+	public enum DoorState {
+		open,
+		closed
+	}
+
+
+	DoorState DoorPosition {
+		get {
+			return _doorState;
+		}
+	}
+
+	[SerializeField]
+	private RaptorAI linkedRaptor;
+
+	private float timer = 0;
+	private Vector3 ClosedPosition;
+
+	private DoorState _doorState = DoorState.closed;
+
+	void Start ()
+	{
+		ClosedPosition = this.gameObject.transform.position;
+	}
+
+	public void ReleaseTheRaptor ()
+	{
+		_doorState = DoorState.open;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (DoorPosition == DoorState.open) {
+			timer += Time.deltaTime;
+
+			this.gameObject.transform.position = Vector3.Slerp (ClosedPosition, ClosedPosition - Vector3.up * 4, (timer / 3.0f));
+
+			if (timer > 3.0f) {
+				linkedRaptor._rState = RaptorAI.RaptorState.HeadingToTarget;
+			}
+		}
+    }
+}
