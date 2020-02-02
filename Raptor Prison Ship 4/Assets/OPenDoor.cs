97216f9fce90c97d334d3c294c668c8b1dc0a9e6
joel <jrattur@gmail.com>
2020-02-02 14:10:05 +1100

    update scene

---
+++ Raptor Prison Ship 4/Assets/OPenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OPenDoor : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        animator.SetInteger("OpenDoor", 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
